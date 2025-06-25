f9418daf7aab43a5db571232b863b0afc2e7fcb2
Ahmed Ashour <ahmed.nasr.ashour@gmail.com>
2025-06-25 22:19:08 +0300

    Change community section image

--- src/components/CommunitySection.tsx
+++ src/components/CommunitySection.tsx
@@ -12,7 +12,7 @@ const CommunitySection = () => {
           <div className="relative">
             <div className="rounded-lg overflow-hidden shadow-xl">
               <img
-                src="https://images.unsplash.com/photo-1559027615-cd4628902d4a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2340&q=80"
+                src="/community/community-13.jpeg"
                 alt="Community development"
                 className="w-full h-96 object-cover"
               />
